17efa30270a07d1295531a491fc0cd776fd91e3f
Francisnasc <38249727+Fjulio2018@users.noreply.github.com>
2024-05-16 18:42:04 -0300

    Vaoltando ao Manager

--- src/test/java/ui_testing/naveen/tests/TestToTest.java
+++ src/test/java/ui_testing/naveen/tests/TestToTest.java
@@ -3,11 +3,18 @@ package ui_testing.naveen.tests;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import ui_testing.naveen.pages.HomePage;
+import ui_testing.naveen.pages.RegisterAccount;
+import java.io.IOException;
 
 import java.io.File;
 import java.io.IOException;
@@ -20,24 +27,9 @@ public class TestToTest {
 
     @BeforeSuite
     public void setUp() {
-        // Define o caminho para o chromedriver
-        String chromeDriverPath = "resources/driver/chromedriver.exe";
-        // Verifica se o chromedriver existe no caminho especificado
-        File chromedriver = new File(chromeDriverPath);
-        if (!chromedriver.exists()) {
-            System.out.println("Erro: chromedriver não encontrado no caminho especificado: " + chromeDriverPath);
-            return; // Aborta a execução se o chromedriver não for encontrado
-        }
-        // Define a propriedade do sistema para o caminho do chromedriver
-        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-
-        // Inicializa o WebDriver do Chrome
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-
-        // Abre uma página da web
         driver.get("https://naveenautomationlabs.com/opencart/");
-
-        // Maximiza a janela do navegador
         driver.manage().window().maximize();
 
         ExtentSparkReporter spark = new ExtentSparkReporter("target/Spark.html");
@@ -47,17 +39,16 @@ public class TestToTest {
     @Test
     public void HomePageToRegisterTest() throws InterruptedException, IOException {
         test = extent.createTest("Validate Home to Register", "initial Validate");
-        // Chamadas para os métodos de teste devem ser adicionadas aqui
-        // Exemplo:
-        // HomePage.validateElements(driver);
-        // HomePage.clickRegisterLink(driver);
-        // RegisterAccount.regLegenda_verify(driver);
+
+         HomePage.validateElements(driver);
+         HomePage.clickRegisterLink(driver);
+         RegisterAccount.regLegenda_verify(driver);
     }
 
-    // Este método pode ser usado para limpar recursos após a execução dos testes
-    // @AfterSuite
-    // public void cleaup() {
-    //    extent.flush();
-    //    driver.quit();
-    // }
+
+     @AfterSuite
+     public void cleaup() {
+        extent.flush();
+        driver.quit();
+     }
 }
