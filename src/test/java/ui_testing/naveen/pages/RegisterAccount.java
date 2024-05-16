568d9cc24fbc8dca08491261fac1eda91cb2fa9b
Francisnasc <38249727+Fjulio2018@users.noreply.github.com>
2024-05-16 15:51:22 -0300

    Crorrecao do problema de conversao de tipos na classe RegisterAccount

--- src/test/java/ui_testing/naveen/pages/RegisterAccount.java
+++ src/test/java/ui_testing/naveen/pages/RegisterAccount.java
@@ -2,7 +2,6 @@ package ui_testing.naveen.pages;
 
 import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.Status;
-import com.aventstack.extentreports.markuputils.Markup;
 import lombok.Getter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -12,7 +11,6 @@ import java.io.IOException;
 import static ui_testing.naveen.tests.TestToTest.*;
 import utils.ScreenShot;
 
-
 public class RegisterAccount {
 
     @Getter
@@ -20,37 +18,24 @@ public class RegisterAccount {
     @Getter
     public static String registerLegendLocator = "//*[@id='account']/legend";
 
-
-
-
     private static final ScreenShot screenShot = new ScreenShot();
-
 
     public static String getActualText(WebDriver driver) {
         WebElement registerLegendElement = driver.findElement(By.xpath(registerLegendLocator));
         return registerLegendElement.getText();
-
     }
 
     public static void regLegenda_verify(WebDriver driver) throws IOException {
-
         String expectedTextRegLegend = "Your Personal Details";
-
         String actualTextRegLegemd = getActualText(driver);
         try {
             Assert.assertEquals(actualTextRegLegemd, expectedTextRegLegend);
-            test.log(Status.PASS, "Test Sucesseful");
-            test.log(Status.PASS, (Markup) test.addScreenCaptureFromBase64String(screenShot.captureScreen(driver, 800, 600)+ "test Passed"));
+            test.log(Status.PASS, "Test Successful");
         } catch (AssertionError e) {
             test.log(Status.FAIL, "The test was not successful. : " + e.getMessage());
-            test.fail(MediaEntityBuilder.createScreenCaptureFromPath(screenShot.captureScreen(driver, 800, 600)).build());
+            String screenshotPath = screenShot.captureScreen(driver, 800, 600);
+            test.fail("Test Failed", MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
             Assert.assertEquals(actualTextRegLegemd, expectedTextRegLegend, "The text on the registration page does not match what was expected.");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
-
-
-
-
 }
